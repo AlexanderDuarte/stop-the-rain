c244fc4b8d7287b8885707578e6e22e5f67050a0
Alexander <alexbfd@hotmail.com>
2025-07-08 19:10:54 +0100

    adjust code to make the game minimally functional

--- main.js
+++ main.js
@@ -1,30 +1,66 @@
 const $gameArea = document.getElementById('gameArea');
-console.log(gameArea);
 
-const element = document.createElement("p");
-element.classList.add("position")
-$gameArea.appendChild(element)
+// const element = document.createElement("p");
+// element.classList.add("position")
+// $gameArea.appendChild(element)
 
 let mousePos = {x: undefined, y: undefined};
+let rain = undefined
 
 
 
 
-$gameArea.addEventListener("mousemove", (e) => {
-    mousePos = {x: e.clientX, y: e.clientY};
-    element.innerHTML = `position x: ${mousePos.x} y: ${mousePos.y}`;
+// $gameArea.addEventListener("mousemove", (e) => {
+//     mousePos = {x: e.clientX, y: e.clientY};
+//     element.innerHTML = `position x: ${mousePos.x} y: ${mousePos.y}`;
+// })
+
+
+$gameArea.addEventListener("click", e => {
+    if(e.target.classList.contains("bubble")){
+$gameArea.removeChild(e.target)
+    }
+    
 })
 
-function createBubble(posX, posY){
-    const bubble = document.createElement("div");
-    bubble.classList.add("bubble");
-    bubble.style.top = `${posY}px`
-    bubble.style.left = `${posX}px`
-    return bubble;
+function startGame(){
+setInterval(() => {
+    createBubble()
+}, 500)
 }
 
-$gameArea.addEventListener("click", (e) => {
-    var rect = element.getBoundingClientRect();
-    $gameArea.appendChild(createBubble(mousePos.x - rect.left - 15, mousePos.y - rect.top - 15))
-    console.log()
+function createBubble(){
+    const bubble = document.createElement("div");
+    bubble.classList.add("bubble");
+    bubble.style.top = "-50px"
+    bubble.style.left = `${Math.floor(Math.random() * 550)}px`
+    $gameArea.appendChild(bubble)
+    let height = -50;
+    
+        
+             const bubbleFall = setInterval(() => {
+                bubble.style.top = height + "px"
+            height += 2
+            if(height > 405){
+                clearInterval(bubbleFall);
+            }
+            }, 16);
+
+}
+
+document.addEventListener("keydown", (e) => {
+    if(e.code === "Enter"){
+        rain = setInterval(() => {
+    createBubble()
+}, 500)
+    }
 })
+
+
+
+
+
+
+
+
+
