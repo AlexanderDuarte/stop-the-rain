a3443ef3445512467162e0912278dcae14b8572a
Alexander <alexbfd@hotmail.com>
2025-07-10 12:36:26 +0100

    Add background, clouds, and frame sprites to game
    Introduced new background, clouds, and frame images to enhance the game's visuals. Updated index.html to include these sprites, refactored bubble creation to use a waterdrop image, and improved CSS for sprite positioning and animation. Also adjusted bubble styling and animation for better appearance and interaction.

--- main.js
+++ main.js
@@ -1,14 +1,12 @@
-const $gameArea = document.getElementById('gameArea');
-const $btnStart = document.getElementById('btnStart');
+const $gameArea = document.getElementById("gameArea");
+const $btnStart = document.getElementById("btnStart");
 
 // const element = document.createElement("p");
 // element.classList.add("position")
 // $gameArea.appendChild(element)
 
-let mousePos = {x: undefined, y: undefined};
-let rain = undefined
-
-
+let mousePos = { x: undefined, y: undefined };
+let rain = undefined;
 
 
 // $gameArea.addEventListener("mousemove", (e) => {
@@ -16,54 +14,48 @@ let rain = undefined
 //     element.innerHTML = `position x: ${mousePos.x} y: ${mousePos.y}`;
 // })
 
+$gameArea.addEventListener(["touchstart"], (e) => {
+  if (e.target.classList.contains("bubble")) {
+    $gameArea.removeChild(e.target);
+  }
+});
 
-$gameArea.addEventListener("touchstart", e => {
-    if(e.target.classList.contains("bubble")){
-$gameArea.removeChild(e.target)
-    }
-    
-})
-
-function startGame(){
-setInterval(() => {
-    createBubble()
-}, 500)
+function startGame() {
+  setInterval(() => {
+    createBubble();
+  }, 500);
 }
 
-function createBubble(){
-    const bubble = document.createElement("div");
-    bubble.classList.add("bubble");
-    bubble.style.top = "-50px"
-    bubble.style.left = `${Math.floor(Math.random() * 550)}px`
-    $gameArea.appendChild(bubble)
-    let height = -50;
-    
-        
-             const bubbleFall = setInterval(() => {
-                bubble.style.top = height + "px"
-            height += 2
-            if(height > 405){
-                clearInterval(bubbleFall);
-            }
-            }, 16);
+function createBubble(gameWidth, gameHeight) {
+  const bubble = document.createElement("div");
+  const img = document.createElement("img");
+  img.src = "./sprites/waterdrop.png";
+  bubble.appendChild(img);
+  bubble.classList.add("bubble");
+  bubble.style.top = "-50px";
+  bubble.style.left = `${Math.floor(Math.random() * ((gameWidth - 100) - 100 + 1) + 70)}px`
+  $gameArea.appendChild(bubble);
+  let height = -50;
 
+  const bubbleFall = setInterval(() => {
+    bubble.style.top = height + "px";
+    height += 2;
+    if (height > gameHeight) {
+      clearInterval(bubbleFall);
+    }
+  }, 16);
 }
 
 $btnStart.addEventListener("click", (e) => {
-    $gameArea.style.display = "block"
-    $btnStart.style.display = "none"
-    
-        rain = setInterval(() => {
-    createBubble()
-}, 500)
-    
-})
+  $gameArea.style.display = "block";
+  $btnStart.style.display = "none";
 
+  const gameAreaSizes = $gameArea.getBoundingClientRect();
+  const gameAreaWidth = gameAreaSizes.width;
+  const gameAreaHeight = gameAreaSizes.height;
+  console.log(gameAreaHeight, gameAreaWidth);
 
-
-
-
-
-
-
-
+  rain = setInterval(() => {
+    createBubble(gameAreaWidth, gameAreaHeight);
+  }, 500);
+});
